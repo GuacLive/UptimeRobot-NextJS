4faad73dccad484d50be67ae63f0318ae95f8dd4
Thomas Lekanger <datagutt@lekanger.no>
2021-04-27 20:40:16 +0200

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
-let uptimeRobotToken = "ur899712-6058a7b7cacc84540a172c95";
+let uptimeRobotToken = "ur851453-fb426aa7a5b187c21762284a";
 // let uptimeRobotToken = "ur899712-6058a7b7cacc84540a172c5";
 
 export { uptimeRobotToken };
